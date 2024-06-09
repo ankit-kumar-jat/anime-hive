9f7e6ac72c10f104c2414fad52aaecc7119abb2b
Ankit Kumar Jat <ankit.kumar@affle.com>
2024-06-09 08:55:58 +0530

    chore: added tailwindcss

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,34 +8,5 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div className="text-3xl font-bold underline">Hello world</div>;
 }
